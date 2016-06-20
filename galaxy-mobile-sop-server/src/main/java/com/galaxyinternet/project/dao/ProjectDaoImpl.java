84ef01af5a9cabab8084e03bb0f6134dda517185
lzj_git <LZJ@LZJ-Galaxy>
2016-06-20 14:39:12 +0800

    新增项目列表分页接口功能(移动端1.3)

--- galaxy-mobile-sop-server/src/main/java/com/galaxyinternet/project/dao/ProjectDaoImpl.java
+++ galaxy-mobile-sop-server/src/main/java/com/galaxyinternet/project/dao/ProjectDaoImpl.java
@@ -3,6 +3,7 @@ package com.galaxyinternet.project.dao;
 import java.util.List;
 import java.util.Map;
 
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Repository;
 import org.springframework.util.Assert;
 
@@ -11,12 +12,16 @@ import com.galaxyinternet.dao.project.ProjectDao;
 import com.galaxyinternet.framework.core.constants.SqlId;
 import com.galaxyinternet.framework.core.dao.impl.BaseDaoImpl;
 import com.galaxyinternet.framework.core.exception.DaoException;
+import com.galaxyinternet.framework.core.model.Page;
 import com.galaxyinternet.framework.core.utils.BeanUtils;
 import com.galaxyinternet.model.project.Project;
+import com.galaxyinternet.vo.ProjectVO;
 
 
 @Repository("projectDao")
 public class ProjectDaoImpl extends BaseDaoImpl<Project, Long> implements ProjectDao {
+	
+	
 	@Override
 	public List<Project> selectProjectByMap(ProjectBo query) {
 		Assert.notNull(query);
@@ -63,6 +68,31 @@ public class ProjectDaoImpl extends BaseDaoImpl<Project, Long> implements Projec
 	@Override
 	public List<Long> queryProjectByUserId(Project project) {
 		return sqlSessionTemplate.selectList(getSqlName("selectByUserId"),project);
+	}
+	
+	public long queryCountProject(ProjectBo query){
+		Assert.notNull(query);
+		try{
+			long count = sqlSessionTemplate.selectOne(getSqlName("selectCountRecordByParam"), query);
+		   return count;
+		}catch(Exception e){
+			throw new DaoException(String.format("根据项目状态统计数据量出错！语句:%s", getSqlName("selectCountRecordByParam")), e);
+		}
+		
+	}
+
+	@Override
+	public Page<ProjectVO> queryPagingList(ProjectBo query , Pageable pageable) {
+		// TODO Auto-generated method stub
+		Assert.notNull(query);	
+		try {
+			List<ProjectVO> contentList = sqlSessionTemplate.selectList(getSqlName("selectPagingProjectList"), getParams(query, pageable));
+					
+			return new  Page<ProjectVO>(contentList, pageable, this.selectCount(query));
+		} catch (Exception e) {
+			throw new DaoException(String.format("根据分页对象查询列表出错！语句:%s", getSqlName("selectPagingProjectList")), e);
+		}
+		
 	}
 
 
