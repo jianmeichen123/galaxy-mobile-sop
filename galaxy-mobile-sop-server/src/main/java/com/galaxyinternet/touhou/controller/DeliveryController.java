9647494143ccf02a7ef0d6ae4ec82ba0022224c2
yuan <chunhuanzhang@galaxyinternet.com>
2016-08-17 13:47:07 +0800

    提交漏提controller

--- galaxy-mobile-sop-server/src/main/java/com/galaxyinternet/touhou/controller/DeliveryController.java
+++ galaxy-mobile-sop-server/src/main/java/com/galaxyinternet/touhou/controller/DeliveryController.java
@@ -5,7 +5,6 @@ package com.galaxyinternet.touhou.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Pageable;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
