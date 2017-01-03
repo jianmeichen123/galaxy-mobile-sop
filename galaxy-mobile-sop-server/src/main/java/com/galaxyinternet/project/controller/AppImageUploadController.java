75a02e56ef0d507271fd8e247131563b068181aa
gxc <xiangchengao@galaxyinternet.com>
2017-01-03 13:21:38 +0800

    修改 上传

--- galaxy-mobile-sop-server/src/main/java/com/galaxyinternet/project/controller/AppImageUploadController.java
+++ galaxy-mobile-sop-server/src/main/java/com/galaxyinternet/project/controller/AppImageUploadController.java
@@ -96,7 +96,7 @@ public class AppImageUploadController extends BaseControllerImpl<SopFile, AppSop
 		 ResponseData<PersonPool> responseBody = new ResponseData<PersonPool>();
 				 	ServletContext s1 = request.getServletContext();
 				 //	String path = s1.getRealPath("/")+"/image";				 	
-				 	String strDirPath = request.getSession().getServletContext().getRealPath("/")+"/image";
+				 	String strDirPath = request.getSession().getServletContext().getRealPath("/")+ File.separator+"image";
 			 		File uploadDirectory = new File(strDirPath); 
 			 		if(!uploadDirectory.exists()){
 			 			uploadDirectory.mkdirs();
