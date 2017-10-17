4d7e014b5ed0106cf2c95e26c18655ea289097dd
liuli <liuli521>
2017-10-17 10:10:51 +0800

    删除全息报告去0的操作

--- galaxy-mobile-sop-server/src/main/java/com/galaxyinternet/hologram/service/InformationTitleServiceImpl.java
+++ galaxy-mobile-sop-server/src/main/java/com/galaxyinternet/hologram/service/InformationTitleServiceImpl.java
@@ -662,7 +662,7 @@ public class InformationTitleServiceImpl extends BaseServiceImpl<InformationTitl
 					}*/
 					
 					//去0操作
-					if(item.getContentDescribe1()!=null){
+					/*if(item.getContentDescribe1()!=null){
 						String s= item.getContentDescribe1();
 						if(s.indexOf(".") > 0){  
 				            s = s.replaceAll("0+?$", "");//去掉多余的0  
@@ -677,7 +677,8 @@ public class InformationTitleServiceImpl extends BaseServiceImpl<InformationTitl
 				            s = s.replaceAll("[.]$", "");//如最后一位是.则去掉  
 				        }
 						item.setContentDescribe2(s);
-					}
+					}*/
+					
 					tempList.add(item);
 				}
 			}
@@ -761,7 +762,7 @@ public class InformationTitleServiceImpl extends BaseServiceImpl<InformationTitl
 			    	}
 			    }
 			    //去0操作
-				if(item.getField5()!=null){
+				/*if(item.getField5()!=null){
 					String s= item.getField5();
 					if(s.indexOf(".") > 0){  
 			            s = s.replaceAll("0+?$", "");//去掉多余的0  
@@ -776,7 +777,7 @@ public class InformationTitleServiceImpl extends BaseServiceImpl<InformationTitl
 			            s = s.replaceAll("[.]$", "");//如最后一位是.则去掉  
 			        }
 					item.setField2(s);
-				}
+				}*/
 
 				title = titleMap.get(item.getTitleId()+"");
 				if(title != null)
