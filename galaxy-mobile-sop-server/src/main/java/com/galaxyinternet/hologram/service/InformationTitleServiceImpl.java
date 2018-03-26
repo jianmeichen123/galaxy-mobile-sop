bc12b7fe9b84688203601531d7398e1b4d601feb
liuli <liuli521>
2018-03-26 09:44:37 +0800

    rc环境

--- galaxy-mobile-sop-server/src/main/java/com/galaxyinternet/hologram/service/InformationTitleServiceImpl.java
+++ galaxy-mobile-sop-server/src/main/java/com/galaxyinternet/hologram/service/InformationTitleServiceImpl.java
@@ -67,7 +67,7 @@ public class InformationTitleServiceImpl extends BaseServiceImpl<InformationTitl
 	
 	//httpClient 需要连接的路径
 
-	private static String url="http://fx.qa.galaxyinternet.com/authority_service" ;
+	private static String url="http://fx.rc.galaxyinternet.com/authority_service" ;
 	
 	// ===  TODO 字典功能
 	
